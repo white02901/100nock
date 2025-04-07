9e3b2b5c3efd6af9d78a69549c3e3f46ac07c41b
Naoaki Okazaki <okazaki@c.titech.ac.jp>
2025-04-07 21:54:46 +0900

    Added a tool for uploading.

--- tools/import.sh
+++ tools/import.sh
@@ -3,5 +3,6 @@ SRCDIR='../nlp100-2025'
 rsync -avh $SRCDIR/data ./
 cp $SRCDIR/tools/build.sh ./tools/
 cp $SRCDIR/tools/modify.py ./tools/
+cp $SRCDIR/tools/upload.sh ./tools/
 cp -r $SRCDIR/ja ./
 jupyter nbconvert --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags answer --TagRemovePreprocessor.remove_all_outputs_tags answer --TagRemovePreprocessor.remove_input_tags answer --to notebook --inplace ja/*.ipynb
